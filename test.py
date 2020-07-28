4636b5bacbda6df84b8b957d022a477058647622
Philip Derbeko <philip.derbeko@gmail.com>
2020-07-27 23:51:38 -0400

    output graphs for tests

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import relu_activation
 import sigmoid_activation
 import settings
 
+import matplotlib.pyplot as plt
+import seaborn as sbs
+
+sbs.set()
+
 num_examples = 1000
 max_value = 100
 epsilon = 0.01
@@ -40,13 +45,24 @@ def run_test(test_set, approx_func, orig_func, test_name):
     print("The average error (%s test) is: %.4f" % (test_name, average_error))
 
 
+    real_results = [orig_func(x) for x in test_set]
+    approx_results = [approx_func(x) for x in test_set]
+    sbs.set_style("whitegrid", {"axes.grid": True})
+    plt.figure()
+    plt.grid(b=True)
+    plt.plot(test_set, real_results, label="real")
+    plt.plot(test_set, approx_results, label="approximation")
+    plt.legend(fancybox=True)
+    plt.title(test_name)
+    plt.savefig(test_name)
+
 def create_test_set(number_examples, border):
     res = [None] * number_examples
     for i in range(0, number_examples):
         res[i] = (random.uniform(0, 1) * border*2)-border
 
+    res.sort()
     return res
-
 
 if __name__ == "__main__":
     main()
